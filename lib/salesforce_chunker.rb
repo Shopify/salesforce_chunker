2aefb0556199388695e889bd1eaa3a977ac0c478
Curtis Holmes <curtis.holmes@shopify.com>
2018-05-10 13:13:05 -0400

    logging implementation

--- lib/salesforce_chunker.rb
+++ lib/salesforce_chunker.rb
@@ -1,5 +1,6 @@
 require "salesforce_chunker/connection.rb"
 require "salesforce_chunker/job.rb"
+require 'logger'
 
 module SalesforceChunker
   class Client
@@ -9,14 +10,18 @@ module SalesforceChunker
       @retry_seconds = retry_seconds
     end
 
-    def query(soql="SELECT Name FROM Account", batch_size=10000, entity="Account")
+    def query(soql="SELECT Name FROM Account", batch_size=10000, entity="Account", logger=nil, log_output=nil)
+      logger ||= Logger.new(log_output)
+      tag = "[salesforce_chunker]"
 
       raise StandardError.new("no block given") unless block_given?
 
+      logger.info("#{tag} Initializing Query")
       job = SalesforceChunker::Job.new(@connection, soql, batch_size, entity)
       retrieved_batches = []
 
       while true
+        logger.info("#{tag} Retrieving batch status information")
         job.get_batch_statuses.each do |batch|
           next if retrieved_batches.include?(batch["id"])
 
@@ -26,12 +31,15 @@ module SalesforceChunker
           when "Completed"
             raise StandardError.new("records failed") if batch["numberRecordsFailed"] > 0
             if batch["numberRecordsProcessed"] > 0
+              logger.info("#{tag} Batch #{retrieved_batches.length + 1} of #{job.batches_count || '?'}: #{batch["numberRecordsProcessed"]} records: retrieving")
               job.retrieve_batch_results(batch["id"]).each do |result_id|
                 job.retrieve_results(batch["id"], result_id).each do |result|
                   result.tap { |h| h.delete("attributes") }
                   yield(result)
                 end
               end
+            else
+              logger.info("#{tag} Batch #{retrieved_batches.length + 1} of #{job.batches_count}: 0 records: skipping")
             end
             retrieved_batches.append(batch["id"])
           when "Failed"
@@ -41,6 +49,8 @@ module SalesforceChunker
 
         break if job.batches_count && retrieved_batches.length == job.batches_count
         # timeout after some period?
+
+        logger.info("#{tag} Waiting #{@retry_seconds} seconds")
         sleep(@retry_seconds)
       end
     end
