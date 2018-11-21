1759ac30503108e1c9bc552a8f5578232e0829df
Curtis Holmes <curtis.holmes@shopify.com>
2018-11-21 15:09:05 -0500

    a lot of work

--- lib/salesforce_chunker/manual_chunking_breakpoint_query.rb
+++ lib/salesforce_chunker/manual_chunking_breakpoint_query.rb
@@ -2,7 +2,12 @@ module SalesforceChunker
   class ManualChunkingBreakpointQuery < Job
 
     def initialize(connection:, object:, operation:, query:, **options)
-       super(connection: connection, object: object, operation: operation, **options)
+      super(connection: connection, object: object, operation: operation, **options)
+
+      @log.info "Creating Breakpoint Query"
+      @batch_id = create_batch(options[:query])
+      @batches_count = 1
+      close
     end
 
 
@@ -26,7 +31,24 @@ module SalesforceChunker
     end
 
 
+    def create_batch(payload)
+      @log.info "Creating Id Batch: \"#{payload.gsub(/\n/, " ").strip}\""
+      response = @connection.post("job/#{@job_id}/batch", payload.to_s, {"Content-Type": "text/csv"})
+      response["batchInfo"]["id"]
+    end
 
+    def retrieve_batch_results
+      response = super
+      if response["result_list"]["result"].is_a? Array
+        response["result_list"]["result"]
+      else
+        [response["result_list"]["result"]]
+      end
+    end
+
+    def get_batch_statuses
+      {"batchInfo" => [super["batchInfoList"]["batchInfo"]]}
+    end
 
     def create_job(object, options)
       body = {
