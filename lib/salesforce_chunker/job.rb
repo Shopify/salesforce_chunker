68345d8326a636b94960b6ae070e1d4506dfb6b7
Curtis Holmes <curtis.holmes@shopify.com>
2018-11-21 15:01:28 -0500

    job json

--- lib/salesforce_chunker/job.rb
+++ lib/salesforce_chunker/job.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 module SalesforceChunker
   class Job
     attr_reader :batches_count
@@ -56,7 +58,9 @@ module SalesforceChunker
 
     def get_batch_results(batch_id)
       retrieve_batch_results(batch_id).each do |result_id|
-        retrieve_results(batch_id, result_id).each do |result|
+        results = retrieve_results(batch_id, result_id)
+        parsed_results = JSON.parse(results)
+        parsed_results.each do |result|
           result.tap { |h| h.delete("attributes") }
           yield(result)
         end
@@ -82,7 +86,7 @@ module SalesforceChunker
     end
 
     def retrieve_results(batch_id, result_id)
-      @connection.get_json("job/#{@job_id}/batch/#{batch_id}/result/#{result_id}")
+      @connection.get("job/#{@job_id}/batch/#{batch_id}/result/#{result_id}")
     end
 
     def close
