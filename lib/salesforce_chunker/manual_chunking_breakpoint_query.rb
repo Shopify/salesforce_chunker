5f5d2cd842c1798313df8bf811dd703e9afaf3ed
Curtis Holmes <curtis.holmes@shopify.com>
2018-11-21 14:59:52 -0500

    initial work

---
+++ lib/salesforce_chunker/manual_chunking_breakpoint_query.rb
@@ -0,0 +1,41 @@
+module SalesforceChunker
+  class ManualChunkingBreakpointQuery < Job
+
+    def initialize(connection:, object:, operation:, query:, **options)
+       super(connection: connection, object: object, operation: operation, **options)
+    end
+
+
+    def get_batch_results(batch_id)
+      retrieve_batch_results(batch_id).each do |result_id|
+        results = retrieve_results(batch_id, result_id)
+
+        lines = results.each_line
+        headers = lines.next
+
+        loop do
+          begin
+            (batch_size-1).times do { lines.next }
+            yield(lines.next.chomp.gsub("\"", ""))
+          rescue StopIteration
+            break
+          end
+        end
+      end
+
+    end
+
+
+
+
+    def create_job(object, options)
+      body = {
+        "operation": @operation,
+        "object": object,
+        "contentType": "CSV",
+      }
+      body[:externalIdFieldName] = options[:external_id] if @operation == "upsert"
+      @connection.post_json("job", body, options[:headers].to_h)["id"]
+    end
+  end
+end
