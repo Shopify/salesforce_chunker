c995ff3b1ad4c3bf49b11ac0bb2682c876ddc850
Curtis Holmes <curtis.holmes@shopify.com>
2018-11-21 14:08:57 -0500

    rewrite to have 2 jobs

--- lib/salesforce_chunker/manual_chunking_query.rb
+++ lib/salesforce_chunker/manual_chunking_query.rb
@@ -2,34 +2,34 @@ module SalesforceChunker
   class ManualChunkingQuery < Job
 
     def initialize(connection:, object:, operation:, query:, **options)
-      batch_size = options[:batch_size] || 100000
+
       where_clause = self.class.query_where_clause(query)
+      batch_size = options[:batch_size] || 100000
+
+      @log.info "Using Manual Chunking"
+      @log.info "Retrieving Ids from records"
+
+      job = SalesforceChunker::SingleBatchJob.new(
+        connection: connection,
+        object: object,
+        operation: operation,
+        query: "Select Id From #{object} #{where_clause} Order By Id Asc",
+      )
+
+      breakpoints = []
+
+      job.download_results(retry_seconds: 10).with_index do |result, i|
+        if i % batch_size == 0 && i != 0
+          breakpoints << result["Id"]
+        end
+      end
 
       super(connection: connection, object: object, operation: operation, **options)
-      @log.info "Using Manual Chunking"
-
-      @log.info "Retrieving Ids from records"
-      breakpoints = breakpoints(object, where_clause, batch_size)
 
       @log.info "Creating Query Batches"
       create_batches(query, breakpoints, where_clause)
 
       close
-    end
-
-    def get_batch_statuses
-      batches = super
-      batches.delete_if { |batch| batch["id"] == @initial_batch_id && batches.count > 1 }
-    end
-
-    def breakpoints(object, where_clause, batch_size)
-      @batches_count = 1
-      @initial_batch_id = create_batch("Select Id From #{object} #{where_clause} Order By Id Asc")
-
-      download_results(retry_seconds: 10)
-        .with_index
-        .select { |_, i| i % batch_size == 0 && i != 0 }
-        .map { |result, _| result["Id"] }
     end
 
     def create_batches(query, breakpoints, where_clause)
