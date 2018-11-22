6d484aae1b95055aa5dd1c7875685de157c26021
Curtis Holmes <curtis.holmes@shopify.com>
2018-11-22 15:44:22 -0500

    starting on tests

--- test/lib/manual_chunking_breakpoint_query_test.rb
+++ test/lib/manual_chunking_breakpoint_query_test.rb
@@ -3,14 +3,50 @@ require "test_helper"
 class ManualChunkingBreakpointQueryTest < Minitest::Test
 
   def setup
-    SalesforceChunker::ManualChunkingQuery.any_instance.stubs(:create_job)
-    SalesforceChunker::ManualChunkingQuery.any_instance.stubs(:breakpoints)
-    SalesforceChunker::ManualChunkingQuery.any_instance.stubs(:close)
+    SalesforceChunker::ManualChunkingBreakpointQuery.any_instance.stubs(:create_job)
+    SalesforceChunker::ManualChunkingBreakpointQuery.any_instance.stubs(:create_batch)
+    SalesforceChunker::ManualChunkingBreakpointQuery.any_instance.stubs(:close)
     @job = SalesforceChunker::ManualChunkingBreakpointQuery.new(connection: nil, object: nil, operation: "query", query: "")
-    SalesforceChunker::ManualChunkingQuery.any_instance.unstub(:create_job)
-    SalesforceChunker::ManualChunkingQuery.any_instance.unstub(:create_batch)
-    SalesforceChunker::ManualChunkingQuery.any_instance.unstub(:close)
+    SalesforceChunker::ManualChunkingBreakpointQuery.any_instance.unstub(:create_job)
+    SalesforceChunker::ManualChunkingBreakpointQuery.any_instance.unstub(:create_batch)
+    SalesforceChunker::ManualChunkingBreakpointQuery.any_instance.unstub(:close)
     @job.instance_variable_set(:@job_id, "3811P00000EFQiYQAX")
   end
 
+  def test_create_batch
+    connection = mock()
+    connection.expects(:post).with(
+      "job/3811P00000EFQiYQAX/batch",
+      "Select Id From Object__c",
+      {"Content-Type": "text/csv"}
+    ).returns({"batchInfo" => 
+      {"id"=> "55024000002iETSAA2", "state"=> "Queued"},
+    })
+
+    @job.instance_variable_set(:@connection, connection)
+
+    assert_equal "55024000002iETSAA2", @job.create_batch("Select Id From Object__c")
+  end
+
+  def test_retrieve_batch_results_array
+    SalesforceChunker::Job.any_instance.expects(:retrieve_batch_results).with("55024000002iETSAA2").returns({
+      "result_list" => 
+      { 
+        "result" => 
+        [
+          "3811P00000EFQiaQAX",
+          "3811P00000EFQibQAX",
+          "3811P00000EFQicQAX",
+        ]
+      }
+    })
+
+    expected = ["3811P00000EFQiaQAX", "3811P00000EFQibQAX", "3811P00000EFQicQAX"]
+
+    assert_equal expected, @job.retrieve_batch_results("55024000002iETSAA2")
+  end
+
+  def test_retrieve_batch_results_single_response
+
+  end
 end
